22ae881710cebb30be5aba3831c8ca1de17ece55
ushan fernando <ushansankalpafernando@gmail.com>
2019-07-18 12:17:23 +0530

    version 0.0.1

--- app/src/main/java/comlab/example/student/lab2/MainActivity.java
+++ app/src/main/java/comlab/example/student/lab2/MainActivity.java
@@ -1,7 +1,9 @@
 package comlab.example.student.lab2;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView tv;
+        tv = findViewById(R.id.text_1);
+
+        CountDownTimer timer = new CountDownTimer(10000,1000) {
+            int time = 0;
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+                tv.setText(String.valueOf(time++));
+            }
+
+            @Override
+            public void onFinish() {
+
+                tv.setText("Modaya...........");
+            }
+        }.start();
+
     }
 }
